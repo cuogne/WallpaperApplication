af7413e1005ca6003ea23b2c44e9e44311e8e886
cuogne <cuog.nc116@gmail.com>
2025-12-16 00:32:21 +0700

    feat: create adapter for photo and basic UI photo

---
+++ app/src/main/java/com/cuogne/wallpaperapplication/ui/adapter/PhotoAdapter.kt
@@ -0,0 +1,46 @@
+package com.cuogne.wallpaperapplication.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.cuogne.wallpaperapplication.R
+import com.cuogne.wallpaperapplication.data.model.PhotoModel
+
+class PhotoAdapter(
+    private val listPhotos: List<PhotoModel>,
+    private val onItemClick: ((PhotoModel) -> Unit)? = null
+) : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.custom_photo, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        val selectedPhoto = listPhotos[position]
+
+        Glide.with(holder.itemView.context)
+            .load(selectedPhoto.urls?.raw)
+            .into(holder.photo)
+
+        holder.itemView.setOnClickListener {
+            onItemClick?.invoke(selectedPhoto)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listPhotos.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val photo: ImageView = itemView.findViewById(R.id.photo)
+    }
+}
--- app/src/main/java/com/cuogne/wallpaperapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/cuogne/wallpaperapplication/ui/main/MainActivity.kt
@@ -3,11 +3,15 @@ package com.cuogne.wallpaperapplication.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.cuogne.wallpaperapplication.R
+import com.cuogne.wallpaperapplication.ui.adapter.PhotoAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var recyclerViewPhoto: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +20,13 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         viewModel.photos.observe(this){ listPhoto ->
-            //
+            recyclerViewPhoto = findViewById(R.id.recyclerViewPhoto)
+
+            val adapter = PhotoAdapter(listPhoto)
+            val layout = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            recyclerViewPhoto.layoutManager = layout
+            layout.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+            recyclerViewPhoto.adapter = adapter
         }
 
         viewModel.getPhotos()
